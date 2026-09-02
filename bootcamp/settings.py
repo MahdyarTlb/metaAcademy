503615dc079b48b328d5fb8664d7cdde0187558d
Mahdyar Talebi <talebimahdyar8@gmail.com>
2026-09-02 22:13:55 +0330

    debug media

--- bootcamp/settings.py
+++ bootcamp/settings.py
@@ -116,8 +116,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/public/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public', 'media')
 
 CSRF_TRUSTED_ORIGINS = [
     'https://mtaacademy.ir',
